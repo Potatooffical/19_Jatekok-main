da3b649d6f32f422126946b4dbea8765028a1e2d
Niffty Its_me_ <danorb12@gmail.com>
2025-11-18 13:16:03 +0100

    modositas

--- frontend/src/Jatekmodosit/Modosit.js
+++ frontend/src/Jatekmodosit/Modosit.js
@@ -1,10 +1,44 @@
+import { useState, useEffect } from "react"
+import Cim from "../Cim"
 
-const Modosit = ({ kivalasztott }) => {
-    return (
+const Modosit = ({ kivalasztott, setkivalasztott }) => {
+    const [adatok, setAdatok] = useState([])
+    const [tolt, setTolt] = useState(true)
+    const [hiba, setHiba] = useState(false)
+    
+    useEffect(() => {
+        const leToltes = async () => {
+        try {
+            const response = await fetch(Cim.Cim + "/jatekegy/" + kivalasztott)
+            const data = await response.json()
+            if (response.ok) {
+                setAdatok(data)
+                setTolt(false)
+            } else {
+                setHiba(true)
+                setTolt(false)
+            }
+        } catch (error) {
+            console.log(error)
+            setHiba(true)
+        }
+    }
+        leToltes()
+    }, [kivalasztott])
+
+    if (tolt)
+        return (
+            <div style={{ textAlign: "center" }}>Adatok betöltése folyamatban...</div>
+        )
+    else if (hiba)
+        return (
+            <div>Hiba</div>
+        )
+
+    else return (
         <div>
-            <p>Modosit</p>
-            <p>{kivalasztott}</p>
+           
         </div>
-    );
-};
-export default Modosit;
+    )
+}
+export default Modosit
